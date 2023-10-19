40499cb3be8307e1a56401968037aa21c10a8479
HarukiYamakawa <visuhalkey@gmail.com>
2023-10-20 07:51:59 +0900

    add rails test  analyze-tool debug

--- rails/app/controllers/api/v1/health_check_controller.rb
+++ rails/app/controllers/api/v1/health_check_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::HealthCheckController < ApplicationController
-    def index
-        current_time = Time.now
-        render json: { 
-            message: "helth_check OK", 
-            timestamp: current_time 
-            }, status: :ok
-    end
+  def index
+    current_time = Time.zone.now
+      render json: {
+        message: 'helth_check OK',
+        timestamp: current_time
+      }, status: :ok
+  end
 end
